ffb0f7f4a4d4f4098a43243a4982b2b906594928
Kyle <kyle@coffeynews.com>
2017-02-21 21:04:50 -0600

    'o' removed from version string

--- src/picross/Graphics.java
+++ src/picross/Graphics.java
@@ -23,7 +23,7 @@ import static picross.Main.*;
 
 public class Graphics implements Runnable, KeyListener, WindowListener {
 
-	private static final String VERSION = "v1.4o";
+	private static final String VERSION = "v1.4";
 	static int bSize;
 	private static int numFrames = 0;
     private final int MIN_BSIZE = 14;
